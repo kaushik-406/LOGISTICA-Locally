05be77cfc0c10b75202a85a9e18c2d3b6d0e53bb
kaushik.khara <kaushik.khara@teknopoint.in>
2022-07-19 18:54:38 +0530

    scrpit remove

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,10 +1,17 @@
+var digitalData = {};
+digitalData = {
+  ctaTitle:"",
+  ctaText:""
+}
 function allctaInteraction(ctaTitle, ctaText) {
   try {
-    (digitalData.event.eventContext = digitalData.event.eventContext || {}),
-      (digitalData.consumer = digitalData.consumer || {});
-    (digitalData.event.eventContext.ctaTitle = ctaTitle || "formSubmit"),
-      (digitalData.event.eventContext.ctaText = ctaText || "Submit"),
-      (digitalData.event.eventName = "all-cta-interaction"),
+    // (digitalData.event.eventContext = digitalData.event.eventContext || {}),
+    //   (digitalData.consumer = digitalData.consumer || {});
+    // (digitalData.event.eventContext.ctaTitle = ctaTitle || "formSubmit"),
+    //   (digitalData.event.eventContext.ctaText = ctaText || "Submit"),
+    //   (digitalData.event.eventName = "all-cta-interaction"),
+    (digitalData.ctaText = ctaText),
+    (digitalData.ctaTitle = ctaTitle),
       callSatellite("all-cta-interaction");
   } catch (err) {}
 }
