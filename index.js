79ff01ee18e7c15b8762b0cc152b9026f185018b
kaushik.khara <kaushik.khara@teknopoint.in>
2022-07-19 13:41:45 +0530

    partytown in meta & AA

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ app.get("/", function (req, res) {
 
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
-  res.set({
-    'Content-Type': 'text/plain',
-    'Content-Length': '123',
-    'ETag': '12345',
-    'Access-Control-Allow-Origin' : '*',
-  })
+  // res.set({
+  //   'Content-Type': 'text/plain',
+  //   'Content-Length': '123',
+  //   'ETag': '12345',
+  //   'Access-Control-Allow-Origin' : '*',
+  // })
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 });
 app.listen(port, function (err) {
   if (err) {
