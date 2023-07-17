0db487b264be25e12bbab0f49348e5ba8d03bb6f
kaushik.khara <kaushik.khara@teknopoint.in>
2023-07-17 13:22:57 +0530

    new INp code added

---
+++ public/js/interactinID.js
@@ -0,0 +1,42 @@
+// // The key is the interaction ID.
+// let = eventLatencies = {};
+
+// const observer = new PerformanceObserver((list) => {
+//   list.getEntries().forEach((entry) => {
+//     if (entry.interactionId > 0) {
+//       const interactionId = entry.interactionId;
+//       if (!eventLatencies.has(interactionId)) {
+//         eventLatencies[interactionId] = [];
+//       }
+//        eventLatencies[interactionId].push(entry.duration);
+//   });
+// });
+
+// observer.observe({type: "event", buffered: true});
+
+// // Log events with maximum event duration for a user interaction
+// Object.entries(eventLatencies).forEach(([k, v]) => {
+//     console.log(Math.max(...v));
+// });
+
+
+let eventLatencies = {};
+
+const observer = new PerformanceObserver((list) => {
+  list.getEntries().forEach((entry) => {
+    if (entry.interactionId > 0) {
+      const interactionId = entry.interactionId;
+      if (!eventLatencies.hasOwnProperty(interactionId)) {
+        eventLatencies[interactionId] = [];
+      }
+      eventLatencies[interactionId].push(entry.duration);
+    }
+  });
+});
+
+observer.observe({ type: "first-input", buffered: true });
+
+// Log events with maximum event duration for a user interaction
+Object.entries(eventLatencies).forEach(([k, v]) => {
+  console.log(Math.max(...v));
+});
