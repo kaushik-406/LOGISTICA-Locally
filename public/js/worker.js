09d030e2a6c05356ee535a826cd8269929698ebe
kaushik.khara <kaushik.khara@teknopoint.in>
2023-08-23 13:48:59 +0530

    remove code for fake DOM for jquery

--- public/js/worker.js
+++
@@ -1,9 +0,0 @@
-// worker.js (your worker script)
-self.addEventListener('message', (event) => {
-    const data = event.data;
-    // Perform some background task with the data
-    const result = data * 2;
-    // Send the result back to the main thread
-    self.postMessage(result);
-  });
-  
